aafa063944e99332c84437a2e6663da01495ad38
aturne13 <allanct.turn@gmail.com>
2020-06-29 20:58:28 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+# if applicant has high income OR good credit
+#     Eligible for loan
+
+has_good_credit = True
+has_criminal_record = True
+
+if has_good_credit and not has_criminal_record:
+    print("Eligible for loan")
