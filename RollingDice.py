c94fe22ad68a068a888d77b7d305c4a9a2ad83ab
aturne13 <allanct.turn@gmail.com>
2020-06-29 20:58:25 -0400

    Update RollingDice.py

--- RollingDice.py
+++ RollingDice.py
@@ -1,4 +1,5 @@
 import random
+user score
 
 computer_dice_1 = random.randrange(1,10)
 computer_dice_2 = random.randrange(1,10)
@@ -10,10 +11,15 @@ print(computer_dice_2)
 user_dice_1 = input("Choose a number between 1-10 exclusively: ")
 user_dice_2 = input("Choose a number between 1-10 exclusively: ")
 
-print("User's choice")
-print(user_dice_1)
-print(user_dice_2)
 
+def compareDice(dice1, dice2):
+    # if dice1 > dice2
+# Create a function that will compare a dice for the user to the computer and return:
+#     a 2 points if the user has higher values
+#     1 point if the user has the same vlaues
+#     0 points if the user has lower values.
+# Do this for each of the die pair (user, computer)
+# Display points at the end. May loop this
 
 """
 # NOTE:
